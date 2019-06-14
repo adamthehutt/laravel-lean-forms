57616b66ab353a992f2ba8d251c6829d19304f6c
adamthehutt <adam.huttler@fracturedatlas.org>
2019-06-14 09:31:02 -0400

    conditionally apply error class

--- resources/views/bootstrap3/text.blade.php
+++ resources/views/bootstrap3/text.blade.php
@@ -1,4 +1,4 @@
-<div class="form-group">
+<div class="form-group @error($name) has-error @enderror">
     <label for="{{ $name }}" class="{{ $label_class }}">{{ $label ?? ucwords(str_replace("_", " ", $name)) }}</label>
     <input id="{{ $name }}" type="text" name="{{ $name }}" value="{{ old($name) ?? optional($__model)->$name ?? $default }}" {!! $attributes !!} class="{{ $class }}">
 </div>
