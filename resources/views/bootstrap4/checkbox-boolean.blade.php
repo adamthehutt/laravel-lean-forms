3ce7d0c2c6b57b5c6d88d9bd1b311e724006133f
johncowleyhall <johncowleyhall@users.noreply.github.com>
2019-07-11 13:02:47 -0700

    Tweaked checkbox

--- resources/views/bootstrap4/checkbox-boolean.blade.php
+++ resources/views/bootstrap4/checkbox-boolean.blade.php
@@ -4,7 +4,7 @@
   <div class="form-check">
     @if($includeLabel)
     <input type="checkbox" name="{{ $name }}" value="1" class="form-check-input {{ $class }}" {!! $attributes !!} @if($__value)checked @endif>
-    <label for="{{ $name }}" class="form-check-label col-form-label {{ $label_class }}">{{ $label }}</label>
+    <label for="{{ $name }}" class="form-check-label {{ $label_class }}">{{ $label }}</label>
     @endif
     <input type="hidden" name="{{ $name }}" value="0">
   </div>
