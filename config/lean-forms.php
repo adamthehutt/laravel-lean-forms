c374c64c45dfc1219b5f9e1039a1bb54cee4e225
johncowleyhall <johncowleyhall@users.noreply.github.com>
2019-07-10 18:13:12 -0700

    Undid change to lean-forms.php, changed bootstrap4/select.blade.php to use bootstrap4

--- config/lean-forms.php
+++ config/lean-forms.php
@@ -2,6 +2,5 @@
 declare(strict_types=1);
 
 return [
-    // "skin" => env("LEAN_FORMS_SKIN", "bootstrap3")
-    "skin" => env("LEAN_FORMS_SKIN", "bootstrap4")
+    "skin" => env("LEAN_FORMS_SKIN", "bootstrap3")
 ];
