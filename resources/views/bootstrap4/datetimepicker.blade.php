f7c7cb453c9dbb05e0004c80e7ea581d301d46b5
johncowleyhall <johncowleyhall@users.noreply.github.com>
2019-07-11 12:22:13 -0700

    .fas > /fa

--- resources/views/bootstrap4/datetimepicker.blade.php
+++ resources/views/bootstrap4/datetimepicker.blade.php
@@ -6,7 +6,7 @@
     @endif
     <div class="input-group date datetimepicker">
       <div class="input-group-prepend">
-        <span class="input-group-text"><i class="fas fa-calendar"></i></span>
+        <span class="input-group-text"><i class="fa fa-calendar"></i></span>
       </div>
         <input id="{{ $id ?? $name }}" type="text" name="{{ $name }}" value="{{ $__value }}" {!! $attributes !!} class="form-control">
     </div>
