6b731c428061cc469e8a54dbc510346cef3568dc
johncowleyhall <johncowleyhall@users.noreply.github.com>
2019-07-10 18:42:22 -0700

    Fixed file

--- resources/views/bootstrap4/file.blade.php
+++ resources/views/bootstrap4/file.blade.php
@@ -4,7 +4,7 @@
     @if($includeLabel)
     <label for="{{ $id ?? $name }}" class="{{ $label_class }}">{{ $label }}</label>
     @endif
-    <input id="{{ $id ?? $name }}" type="file" name="{{ $name }}" value="{{ $__value }}" {!! $attributes !!} class="{{ $class }}">
+    <input id="{{ $id ?? $name }}" type="file" name="{{ $name }}" value="{{ $__value }}" {!! $attributes !!} class="form-control-file">
 @if($includeFormGroup)
 </div>
 @endif
