0a21f14e3c804906d850e28d7a5fbaa9cd3aa701
johncowleyhall <johncowleyhall@users.noreply.github.com>
2019-07-17 12:12:47 -0700

    moved class for datetimepicker

--- resources/views/bootstrap4/datetimepicker.blade.php
+++ resources/views/bootstrap4/datetimepicker.blade.php
@@ -4,11 +4,11 @@
     @if($includeLabel)
     <label for="{{ $id ?? $name }}" class="">{{ $label  }}</label>
     @endif
-    <div class="input-group date datetimepicker">
+    <div class="input-group date">
       <div class="input-group-prepend">
         <span class="input-group-text"><i class="fa fa-calendar"></i></span>
       </div>
-        <input id="{{ $id ?? $name }}" type="text" name="{{ $name }}" value="{{ $__value }}" {!! $attributes !!} class="form-control">
+        <input id="{{ $id ?? $name }}" type="text" name="{{ $name }}" value="{{ $__value }}" {!! $attributes !!} class="form-control datetimepicker">
     </div>
 @if($includeFormGroup)
 </div>
