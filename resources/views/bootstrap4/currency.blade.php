c1000ea25ae1f1508dd4d9b4321694b205040524
johncowleyhall <johncowleyhall@users.noreply.github.com>
2019-07-23 15:01:43 -0700

    switched currency FA class to fa-dollar-sign

--- resources/views/bootstrap4/currency.blade.php
+++ resources/views/bootstrap4/currency.blade.php
@@ -7,7 +7,7 @@
     <div class="input-group">
         <div class="input-group-prepend">
             <span class="input-group-text">
-                <i class="fa fa-dollar"></i>
+                <i class="fa fa-dollar-sign"></i>
             </span>
         </div>
         <input id="{{ $name }}" type="text" name="{{ $name }}" value="{{ $__value }}" {!! $attributes !!} class="form-control currency">
