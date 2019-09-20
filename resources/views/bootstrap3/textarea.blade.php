b78546011e8e91d6f56f060ab05c792fc9f4f736
Adam Huttler <adam.huttler@gmail.com>
2019-09-20 09:56:31 -1000

    add error classes to textarea wrappers

--- resources/views/bootstrap3/textarea.blade.php
+++ resources/views/bootstrap3/textarea.blade.php
@@ -1,4 +1,4 @@
-<div class="form-group">
+<div class="form-group @if(isset($errors) && $errors->has($name)) has-error @endif">
     <label class="{{ $label_class }}" for="{{ $name }}">{{ $label }}</label>
     <textarea id="{{ $name }}" name="{{ $name }}" {!! $attributes !!} class="form-control {{ $class }}">{{ $__value }}</textarea>
 </div>
