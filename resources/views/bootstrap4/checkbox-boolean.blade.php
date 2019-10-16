3422528310184dac6347e64aab43bc3edae6de6a
Adam Huttler <adam.huttler@gmail.com>
2019-10-16 12:24:04 -1000

    fix bug with checkbox only being displayed with label

--- resources/views/bootstrap4/checkbox-boolean.blade.php
+++ resources/views/bootstrap4/checkbox-boolean.blade.php
@@ -2,8 +2,8 @@
 <div class="form-group">
 @endif
   <div class="form-check">
-    @if($includeLabel)
     <input type="checkbox" name="{{ $name }}" value="1" id="boolean-{{ $label }}" class="form-check-input {{ $class }}" {!! $attributes !!} @if($__value)checked @endif>
+    @if($includeLabel)
     <label for="boolean-{{ $label }}" class="form-check-label {{ $label_class }}">{{ $label }}</label>
     @endif
     <input type="hidden" name="{{ $name }}" value="0">
