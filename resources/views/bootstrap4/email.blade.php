b9b2451c7221347d6a302476983fafb2ccd8a111
Adam Huttler <adam.huttler@gmail.com>
2019-11-12 14:32:04 -1000

    add label class

--- resources/views/bootstrap4/email.blade.php
+++ resources/views/bootstrap4/email.blade.php
@@ -2,7 +2,7 @@
 <div class="form-group @error($name) has-error @enderror">
 @endif
     @if($includeLabel)
-    <label for="{{ $id ?? $name }}" class="{{ $label_class }}">{{ $label }}</label>
+    <label for="{{ $id ?? $name }}" class="col-form-label {{ $label_class }}">{{ $label }}</label>
     @endif
     <input id="{{ $id ?? $name }}" type="email" name="{{ $name }}" value="{{ $__value }}" {!! $attributes !!} class="form-control {{ $class }}">
 @if($includeFormGroup)
