33a65923e2a209a1095249dde95771951c7abd3d
Adam Huttler <adam.huttler@gmail.com>
2020-01-24 13:44:17 -1000

    consistency with id conventions

--- resources/views/bootstrap4/checkbox-boolean.blade.php
+++ resources/views/bootstrap4/checkbox-boolean.blade.php
@@ -2,9 +2,9 @@
 <div class="form-group">
 @endif
   <div class="form-check">
-    <input type="checkbox" name="{{ $name }}" value="1" id="boolean-{{ $label }}" class="form-check-input {{ $class }}" {!! $attributes !!} @if($__value)checked @endif>
+    <input type="checkbox" name="{{ $name }}" value="1" id="{{ $id ?? $name }}" class="form-check-input {{ $class }}" {!! $attributes !!} @if($__value)checked @endif>
     @if($includeLabel)
-    <label for="boolean-{{ $label }}" class="form-check-label {{ $label_class }}">{{ $label }}</label>
+    <label for="{{ $id ?? $name }}" class="form-check-label {{ $label_class }}">{{ $label }}</label>
     @endif
     <input type="hidden" name="{{ $name }}" value="0">
   </div>
