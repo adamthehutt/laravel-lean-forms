4bb8ce74edbb915eb26b3850a3468ee70acf4723
Adam Huttler <adam.huttler@gmail.com>
2020-01-24 13:56:08 -1000

    Move hidden field before boolean checkbox

--- resources/views/bootstrap4/checkbox-boolean.blade.php
+++ resources/views/bootstrap4/checkbox-boolean.blade.php
@@ -2,11 +2,11 @@
 <div class="form-group">
 @endif
   <div class="form-check">
+    <input type="hidden" name="{{ $name }}" value="0">
     <input type="checkbox" name="{{ $name }}" value="1" id="{{ $id ?? $name }}" class="form-check-input {{ $class }}" {!! $attributes !!} @if($__value)checked @endif>
     @if($includeLabel)
     <label for="{{ $id ?? $name }}" class="form-check-label {{ $label_class }}">{{ $label }}</label>
     @endif
-    <input type="hidden" name="{{ $name }}" value="0">
   </div>
 @if($includeFormGroup)
 </div>
