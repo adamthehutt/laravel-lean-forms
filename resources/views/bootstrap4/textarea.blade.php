a55be1d6d99f43bd88d95d33e0a010d33e8c0b65
Adam Huttler <adam.huttler@gmail.com>
2020-04-03 13:11:42 -1000

    bugfix, support for hiding labels

--- resources/views/bootstrap4/textarea.blade.php
+++ resources/views/bootstrap4/textarea.blade.php
@@ -1,4 +1,10 @@
+@if($includeFormGroup)
 <div class="form-group @if(isset($errors) && $errors->has($name)) has-error @endif">
-    <label class="{{ $label_class }}" for="{{ $name }}">{{ $label }}</label>
+@endif
+    @if($includeLabel)
+    <label for="{{ $name }}" class="col-form-label {{ $label_class }}">{{ $label }}</label>
+    @endif
     <textarea id="{{ $name }}" name="{{ $name }}" {!! $attributes !!} class="form-control {{ $class }}">{{ $__value }}</textarea>
+@if($includeFormGroup)
 </div>
+@endif
